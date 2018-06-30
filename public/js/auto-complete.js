d4a2bcc81bd1d71eba083841e6056a52781ee2d7
Kyle Thomson <idynkydnk@gmail.com>
2018-06-29 17:23:06 -0700

    add marin and Josh

--- public/js/auto-complete.js
+++ public/js/auto-complete.js
@@ -262,7 +262,7 @@ var autolocation = new autoComplete({
 });
 
 
-var all_choices = ["Kyle Thomson","Chris Goshow","Chris Dedo","Evan Murray","Ryan Aikens","Chris Jones","Brady Donahue","Mike Ciav","Alex Amylon","Ike Isenhour","Ryan Donahue","Jeff Zhao","Tyler ???","Louis ???","Vinny ???","Cole Constantino","Hawk Hatcher","Tyler Bird","Brian Bowie","Aaron Plumb","Denis Buznea","Kevin Legg","Kurt Frahn","Ryan Schmidt","Nic Stojak","Mike Kuk","Ian Marvel","Matt Sokolowski","Dustin Rambo","Troy Nowak"]
+var all_choices = ["Kyle Thomson","Chris Goshow","Chris Dedo","Evan Murray","Ryan Aikens","Chris Jones","Brady Donahue","Mike Ciav","Alex Amylon","Ike Isenhour","Ryan Donahue","Jeff Zhao","Josh Roth","Marin Xaimov","Tyler ???","Louis ???","Vinny ???","Cole Constantino","Hawk Hatcher","Tyler Bird","Brian Bowie","Aaron Plumb","Denis Buznea","Kevin Legg","Kurt Frahn","Ryan Schmidt","Nic Stojak","Mike Kuk","Ian Marvel","Matt Sokolowski","Dustin Rambo","Troy Nowak"]
 
 var autowinner1 = new autoComplete({
   selector: '#auto-winner1',
