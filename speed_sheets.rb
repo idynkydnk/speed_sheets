7094a32e2f3567b0183a4bf9d093975841368e9a
Kyle Thomson <idynkydnk@gmail.com>
2018-10-29 16:56:43 -0700

    updated some vollis stats algos

--- speed_sheets.rb
+++ speed_sheets.rb
@@ -53,7 +53,7 @@ get '/' do
 end
 
 get '/vollis' do
-  @vollisgames = Vollis.all :order => :id.desc
+  @vollisgames = Vollisgame.all :order => :id.desc
   @todays_stats = todays_stats
   @min_games = 1
   @max_games = 14
@@ -65,19 +65,19 @@ get '/vollis' do
 end
 
 get '/add_vollis_game' do
-  @vollisgames = Vollis.all :order => :id.desc
+  @vollisgames = Vollisgame.all :order => :id.desc
   @players = Player.all
   players = []
   @players.each do |player|
     players << player.player
   end
   @players = players
-  @todays_stats = todays_stats
+  @todays_vollis_stats = todays_vollis_stats
   erb :add_game
 end
 
 post '/add_vollis_game' do
-  n = Vollis.new
+  n = Vollisgame.new
   n.winner = params[:winner]
   n.loser = params[:loser]
   n.date = my_time_now 
@@ -327,6 +327,49 @@ def all_players
     players << game.winner2 unless players.include?(game.winner2)
     players << game.loser1 unless players.include?(game.loser1) 
     players << game.loser2 unless players.include?(game.loser2) 
+  end
+  return players
+end
+
+def todays_vollis_stats
+  name_and_stats = [] 
+  games = todays_vollis_games
+  players = todays_vollis_players(games)
+  players.each do |player|
+    wins, losses = 0, 0
+    games.each do |game|
+      if player == game.winner
+        wins += 1
+      elsif player == game.loser
+        losses += 1
+      end
+    end
+    win_percent = "%.2f" % (wins.to_f / (wins + losses).to_f * 100.0)
+    x = { :player => player, :wins => wins, :losses => losses, 
+          :win_percentage => win_percent }
+    name_and_stats.push(x)
+  end
+  name_and_stats.sort_by! { |a| a[:win_percentage].to_f}
+  name_and_stats.reverse
+end
+
+def todays_vollis_games
+  games = []
+  @vollisgames.each do |game|
+    if game.date[0..1].to_i == Time.now.month &&
+        game.date[3..4].to_i == Time.now.day &&
+        game.date[6..9].to_i == Time.now.year
+      games << game 
+    end
+  end
+  return games
+end
+
+def todays_vollis_players(games)
+  players = []
+  games.each do |game|
+    players << game.winner unless players.include?(game.winner)
+    players << game.loser unless players.include?(game.loser)
   end
   return players
 end
