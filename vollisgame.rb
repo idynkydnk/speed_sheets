e5903428240e6474a0c92635f084c381f7680aab
Kyle Thomson <idynkydnk@gmail.com>
2018-10-29 20:02:53 -0700

    vollis main stats page good

--- vollisgame.rb
+++ vollisgame.rb
@@ -10,15 +10,15 @@ class Vollisgame
   property :updated_at, DateTime
 end
 
-get '/vollis' do
+get '/vollis_stats' do
   @vollisgames = Vollisgame.all :order => :id.desc
-  @todays_stats = todays_stats
+  @todays_vollis_stats = todays_vollis_stats
   @min_games = 1
-  @max_games = 14
-  @years_stats = years_stats
-  @min_games = 20
+  @max_games = 10
+  @years_stats = years_vollis_stats
+  @min_games = 10
   @max_games = 99999
-  @min_years_stats = years_stats
+  @min_years_stats = years_vollis_stats
   erb :vollis_stats
 end
 
@@ -87,4 +87,35 @@ def todays_vollis_players(games)
     players << game.loser unless players.include?(game.loser)
   end
   return players
+end
+
+def all_vollis_players
+  players = []
+  @vollisgames.each do |game|
+    players << game.winner unless players.include?(game.winner) 
+    players << game.loser unless players.include?(game.loser) 
+  end
+  return players
+end
+
+def years_vollis_stats
+  name_and_stats = []
+  players = all_vollis_players
+  players.each do |player|
+    wins, losses = 0, 0
+    @vollisgames.each do |game|
+      if player == game.winner
+        wins += 1
+      elsif player == game.loser
+        losses += 1
+      end
+    end
+    win_percent = "%.2f" % (wins.to_f / (wins + losses).to_f * 100.0)
+    total_games = wins + losses
+    x = { :player => player, :wins => wins, :losses => losses, 
+          :win_percentage => win_percent, :total_games => total_games }
+    name_and_stats << x unless x[:total_games] < @min_games || x[:total_games] >= @max_games
+  end
+  name_and_stats.sort_by! { |a| a[:win_percentage].to_f}
+  name_and_stats.reverse
 end
